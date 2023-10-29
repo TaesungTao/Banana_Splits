c7944e96783b390b3ec216c00db536323ad7b166
TaesungTao <taesungtaole@gmail.com>
2023-10-29 13:42:26 -0500

    UI pop ups

--- Banana Splits Tech Prototype/Assets/MyScripts/CollisionChangeScene.cs
+++ Banana Splits Tech Prototype/Assets/MyScripts/CollisionChangeScene.cs
@@ -2,16 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class CollisionChangeScene : MonoBehaviour
 {
     public int sceneID; 
+    public Image DoorChange;
     // Start is called before the first frame update
     // followed a unity tutorial on scene change
-    void OnCollisionEnter2D(Collision2D collision)
+    void Start()
     {
-        if (collision.gameObject.tag == "Player"){
+        DoorChange.enabled = false;
+    }
+    
+    void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.attachedRigidbody){
+            DoorChange.enabled = true;
+            if (Input.GetKey(KeyCode.Return)){
+                DoorChange.enabled = false;
         ChangeScenes(sceneID);
+        }
+        }
+    }
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.attachedRigidbody){
+            DoorChange.enabled = false;
         }
     }
     public void ChangeScenes(int sceneID)
